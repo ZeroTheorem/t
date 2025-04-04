d4d94a7af6761012f4f06b2f23d2c13a9e4a2541
q1110 <zeratuliche@gmail.com>
2025-04-04 10:20:11 +0300

    start commit

--- go.mod
+++ go.mod
@@ -2,7 +2,4 @@ module t
 
 go 1.22.2
 
-require (
-	github.com/go-telegram-bot-api/telegram-bot-api v4.6.4+incompatible // indirect
-	github.com/technoweenie/multipartstreamer v1.0.1 // indirect
-)
+require gopkg.in/telebot.v4 v4.0.0-beta.4 // indirect
--- main.go
+++ main.go
@@ -3,69 +3,65 @@ package main
 import (
 	"fmt"
 	"log"
+	"os"
+	"time"
 
-	"github.com/go-telegram-bot-api/telegram-bot-api"
+	tele "gopkg.in/telebot.v4"
 )
 
-var successCount = 0
-var failCount = 0
+const msg = `
+Wins: **%v**; Loses: **%v**
+
+Win rate: **%v**
+`
+
+var winsCount = 22
+var losesCount = 5
 
 // Функция для вычисления процента успеха
 func getSuccessRate() float64 {
-	total := successCount + failCount
+	total := winsCount + losesCount
 	if total == 0 {
 		return 0.0
 	}
-	return (float64(successCount) / float64(total)) * 100
-}
-
-// Функция для формирования ответа бота
-func handleMessage(message string) string {
-	// Проверяем, что сообщение состоит из одного символа + или -
-	switch message {
-	case "+":
-		successCount++
-	case "-":
-		failCount++
-	default:
-		return "Отправь только символы '+' или '-' для инкрементации успеха или провала."
-	}
-
-	// Формируем процент успеха
-	successRate := getSuccessRate()
-
-	// Возвращаем процент успеха и количество успехов и провалов
-	return fmt.Sprintf("Успехов: %d, Провалов: %d\nПроцент успеха: %.2f%%", successCount, failCount, successRate)
+	return (float64(winsCount) / float64(total)) * 100
 }
 
 func main() {
-	// Создаем бота с вашим токеном
-	token := "6951087608:AAGfVdrNduwKwnvUoE5JG_DbPKlc0OQLG9s"
-	bot, err := tgbotapi.NewBotAPI(token)
+	pref := tele.Settings{
+		Token:     os.Getenv("TOKEN"),
+		Poller:    &tele.LongPoller{Timeout: 10 * time.Second},
+		ParseMode: tele.ModeMarkdownV2,
+	}
+
+	b, err := tele.NewBot(pref)
 	if err != nil {
-		log.Panic(err)
+		log.Fatal(err)
+		return
 	}
 
-	// Получаем обновления для бота
-	u := tgbotapi.NewUpdate(0)
-	u.Timeout = 60
+	selector := &tele.ReplyMarkup{}
+	btnWin := selector.Data("Win", "W")
+	btnLose := selector.Data("Lose", "L")
 
-	updates, _ := bot.GetUpdatesChan(u)
+	selector.Inline(
+		selector.Row(btnWin, btnLose),
+	)
 
-	for update := range updates {
-		// Игнорируем, если нет текста в сообщении
-		if update.Message == nil {
-			continue
-		}
+	b.Handle("/start", func(c tele.Context) error {
+		return c.Send("Hello!", selector)
+	})
 
-		// Получаем текст из сообщения
-		msgText := update.Message.Text
+	b.Handle(&btnWin, func(c tele.Context) error {
+		winsCount++
+		winRate := getSuccessRate()
+		return c.Send(fmt.Sprintf(msg, winsCount, losesCount, winRate))
+	})
 
-		// Формируем ответ
-		response := handleMessage(msgText)
-
-		// Отправляем ответ
-		msg := tgbotapi.NewMessage(update.Message.Chat.ID, response)
-		bot.Send(msg)
-	}
+	b.Handle(&btnLose, func(c tele.Context) error {
+		losesCount++
+		winRate := getSuccessRate()
+		return c.Send(fmt.Sprintf(msg, winsCount, losesCount, winRate))
+	})
+	b.Start()
 }
